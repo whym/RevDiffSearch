73c1859344ca90f1a89caad6b8fca214274a61ed
Diederik van Liere <dvanliere@wikimedia.org>
2011-11-04 13:56:50 -0400

    Fixed typo.

--- web/settings.py
+++ web/settings.py
@@ -5,6 +5,6 @@ hostname = socket.gethostname()
 HOST, PORT = "localhost", 9999
 
 if hostname == 'alpha':
-    INDEX_DIR = os.path.jon('/','data-large', 'lucene', 'index')
+    INDEX_DIR = os.path.join('/','data-large', 'lucene', 'index')
 else:
     INDEX_DIR = os.path.join('c:\\','lucene-3.4.0','index')
