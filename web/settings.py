610647c9913971081100046bd0b41b14bab0c7bf
Diederik van Liere <dvanliere@wikimedia.org>
2011-11-07 13:54:17 -0800

    argghh path issue

--- web/settings.py
+++ web/settings.py
@@ -6,6 +6,6 @@ HOST, PORT = "localhost", 9999
 
 if HOSTNAME == 'alpha':
     HOSTNAME = 'production'
-    INDEX_DIR = os.path.join('/','data-large', 'lucene', 'index')
+    INDEX_DIR = os.path.join('/','data-large')
 else:
     INDEX_DIR = os.path.join('c:\\','lucene-3.4.0','index')
