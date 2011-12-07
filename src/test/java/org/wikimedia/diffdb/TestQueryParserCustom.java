c6dc6dc0bada5e3235474550421adf98d04995ad
whym <whym@whym.org>
2011-12-07 17:20:52 +0900

    * fix the order of arguments in assertions, and expected n-grams

--- src/test/java/org/wikimedia/diffdb/TestQueryParserCustom.java
+++ src/test/java/org/wikimedia/diffdb/TestQueryParserCustom.java
@@ -1,10 +1,10 @@
 package org.wikimedia.diffdb;
 
-import org.wikimedia.diffdb.QueryParserCustom;
 import static org.junit.Assert.*;
 
 import org.apache.lucene.search.Query;
-import org.apache.lucene.search.MultiTermQuery;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.TermQuery;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,78 +20,79 @@ public class TestQueryParserCustom {
 	public void testQueryNoExplicitField() {
 		String querystr = "foo";
 		Query query = qp.construct(querystr);
-		assertEquals(query.toString(), "added:foo");
+		assertEquals("added:foo", query.toString());
 	}
 	
 	@Test
 	public void testQueryExplicitField() {
 		String querystr = "title:foo";
 		Query query = qp.construct(querystr);
-		assertEquals(query.toString(), "title:foo");
+		assertEquals(TermQuery.class, query.getClass());
+		assertEquals("title:foo", query.toString());
 	}
 	
 	@Test
 	public void testQueryNoExplicitFieldCompound() {
-		String querystr = "foo bar";
+		String querystr = "foo1 bar2";
 		Query query = qp.construct(querystr);
-		assertEquals(query.getClass(), MultiTermQuery.class);
-		assertEquals(query.toString(), "added:foo oo o b  ba bar");
+		assertEquals(BooleanQuery.class, query.getClass());
+		assertEquals("added:foo added:oo1 added:bar added:ar2", query.toString());
 	}
 	
 	@Test
-	public void testQueryNoExplicitFieldCompoundExect() {
+	public void testQueryNoExplicitFieldCompoundExact() {
 		String querystr = "\"foo bar\"";
 		Query query = qp.construct(querystr);
-		assertEquals(query.toString(), "\"foo oo o b  ba bar\"");
+		assertEquals("\"foo oo o b  ba bar\"", query.toString());
 	}
 	
 	@Test
 	public void testQueryImplicitAddedAndNamespace() {
 		String querystr = "foo namespace:1";
 		Query query = qp.construct(querystr);
-		assertEquals(query.toString(), "foo namespace:1");
+		assertEquals("foo namespace:1", query.toString());
 	}
 	
 	@Test
 	public void testQueryExplicitAddedAndNamespaceSimple() {
 		String querystr = "added:foo namespace:1";
 		Query query = qp.construct(querystr);
-		assertEquals(query.toString(), "added:foo namespace:1");
+		assertEquals("added:foo namespace:1", query.toString());
 	}
 	
 	@Test
 	public void testQueryExplicitAddedAndNamespaceComplex() {
 		String querystr = "added:foo namespace:1";
 		Query query = qp.construct(querystr);
-		assertEquals(query.toString(), "added:foo oo o b  ba bar namespace:1");
+		assertEquals("added:foo oo o b  ba bar namespace:1", query.toString());
 	}
 	
 	@Test
 	public void testQueryImplicitAndTimestamp() {
 		String querystr = "foo timestamp:[2006-01 TO 2007-01]";
 		Query query = qp.construct(querystr);
-		assertEquals(query.toString(), "foo timestamp:[2006-01 TO 2007-01]");
+		assertEquals("foo timestamp:[2006-01 TO 2007-01]", query.toString());
 	}
 	
 	@Test
 	public void testQueryExplicitAddedAndTimestampSimple() {
 		String querystr = "added:foo timestamp:[2006-01 TO 2007-01]";
 		Query query = qp.construct(querystr);
-		assertEquals(query.toString(), "added:foo timestamp:[2006-01 TO 2007-01]");
+		assertEquals("added:foo timestamp:[2006-01 TO 2007-01]", query.toString());
 	}
 
 	@Test
 	public void testQueryExplicitAddedAndTimestampComplex() {
 		String querystr = "added:foo timestamp:[2006-01 TO 2007-01]";
 		Query query = qp.construct(querystr);
-		assertEquals(query.toString(), "added:foo oo o b  ba bar timestamp:[2006-01 TO 2007-01]");
+		assertEquals("added:foo oo o b  ba bar timestamp:[2006-01 TO 2007-01]", query.toString());
 	}
 
 	@Test
 	public void testQueryExplicitAddedAndNotSimple() {
 		String querystr = "added:foo -bar";
 		Query query = qp.construct(querystr);
-		assertEquals(query.toString(), "added:foo -bar");
+		assertEquals("added:foo -bar", query.toString());
 	}
 
 
@@ -99,7 +100,7 @@ public class TestQueryParserCustom {
 	public void testQueryExplicitAddedAndNotComplex() {
 		String querystr = "added:foo -bar bear beer";
 		Query query = qp.construct(querystr);
-		assertEquals(query.toString(), "added:foo -bar -ar -r b -bea -ear -ar  -r b - be -bee -eer");
+		assertEquals("added:foo -bar -ar -r b -bea -ear -ar  -r b - be -bee -eer", query.toString());
 	}
 
 }
