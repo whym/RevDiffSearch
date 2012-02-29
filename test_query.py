940708e357f820318241d6d3e2cf6730b48f903c
Yusuke Matsubara <whym@whym.org>
2012-02-29 11:13:38 +0900

    * test cases for date time arguments

---
+++ test_query.py
@@ -0,0 +1,23 @@
+import unittest
+import urllib
+import tempfile
+import os
+import re
+import StringIO
+from collections import namedtuple
+import timeit
+import sys
+from datetime import datetime
+import query
+
+class TestQuery(unittest.TestCase):
+        
+    def test_partial_datetime_match(self):
+        self.assertTrue(query.partial_datetime_match('%Y-%m-%dT%H:%M:%SZ', '2011'))
+        self.assertTrue(query.partial_datetime_match('%Y-%m-%dT%H:%M:%SZ', '2011-12-01'))
+        self.assertFalse(query.partial_datetime_match('%Y-%m-%dT%H:%M:%SZ', '1-2-34'))
+        self.assertFalse(query.partial_datetime_match('%Y-%m-%dT%H:%M:%SZ', '20110101'))
+        self.assertFalse(query.partial_datetime_match('%Y-%m-%dT%H:%M:%SZ', '20091010'))
+
+if __name__ == '__main__':
+    unittest.main()
