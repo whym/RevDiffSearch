bfeb447c123f243b52fb1046a5ca44f78a3603e7
Yusuke Matsubara <whym@whym.org>
2013-08-10 09:39:16 +0800

    * fix script name

--- tests/test_trend_query.py
+++ tests/test_trend_query.py
@@ -1,14 +1,14 @@
 import unittest
-import query
+import trend_query as tquery
 
 class TestQuery(unittest.TestCase):
         
     def test_partial_datetime_match(self):
-        self.assertTrue(query.partial_datetime_match('%Y-%m-%dT%H:%M:%SZ', '2011'))
-        self.assertTrue(query.partial_datetime_match('%Y-%m-%dT%H:%M:%SZ', '2011-12-01'))
-        self.assertFalse(query.partial_datetime_match('%Y-%m-%dT%H:%M:%SZ', '1-2-34'))
-        self.assertFalse(query.partial_datetime_match('%Y-%m-%dT%H:%M:%SZ', '20110101'))
-        self.assertFalse(query.partial_datetime_match('%Y-%m-%dT%H:%M:%SZ', '20091010'))
+        self.assertTrue(tquery.partial_datetime_match('%Y-%m-%dT%H:%M:%SZ', '2011'))
+        self.assertTrue(tquery.partial_datetime_match('%Y-%m-%dT%H:%M:%SZ', '2011-12-01'))
+        self.assertFalse(tquery.partial_datetime_match('%Y-%m-%dT%H:%M:%SZ', '1-2-34'))
+        self.assertFalse(tquery.partial_datetime_match('%Y-%m-%dT%H:%M:%SZ', '20110101'))
+        self.assertFalse(tquery.partial_datetime_match('%Y-%m-%dT%H:%M:%SZ', '20091010'))
 
 if __name__ == '__main__':
     unittest.main()
