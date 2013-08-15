f54c09f5e6b0434ebf27d87b177a3cdd1e66d081
Yusuke Matsubara <whym@whym.org>
2013-08-15 19:39:03 +0900

    * and more tests

--- src/test/scala/test-searcherdaemon.scala
+++ src/test/scala/test-searcherdaemon.scala
@@ -96,13 +96,15 @@ class SearcherDaemonSpec extends FunSpec {
   }
 
   def n3daemon(doc: String) = daemon(new SimpleNGramAnalyzer(3), doc)
+  def n1daemon(doc: String) = daemon(new SimpleNGramAnalyzer(1), doc)
 
 
   describe("SearcherDaemon with two small documents") {
 
-    val d = n3daemon("""233192	10	0	'Accessiblecomputing'	980043141	u'*'	False	99	u'RoseParks'	0:1:u'This subject covers\\n\\n* AssistiveTechnology\\n\\n* AccessibleSoftware\\n\\n* AccessibleWeb\\n\\n* LegalIssuesInAccessibleComputing\\n\\n'
-18201	12	0	'Anarchism'	1014649222	u'Automated conversion'	True	None	u'Conversion script'	9230:1:u'[[talk:Anarchism|'	9252:1:u']]'	9260:1:u'[[Anarchism'	9276:1:u'|/Todo]]'	9292:1:u'talk:'	9304:-1:u'/Talk'	9464:1:u'\\n'
-""")
+    val doc = """233192	10	0	'Accessiblecomputing'	980043141	u'*'	False	99	u'RoseParks'	0:1:u'This subject covers\\n\\n* AssistiveTechnology\\n\\n* AccessibleSoftware\\n\\n* AccessibleWeb\\n\\n* LegalIssuesInAccessibleComputing\\n\\n'
+18201	12	0	'Anarchism'	1014649222	u'Automated conversion'	True	None	u'Conversion script'	9230:1:u'[[talk:Anarchism|'	9252:1:u']]'	9260:1:u'[[Anarchism'	9276:1:u'|/Todo]]'	9292:1:u'This covers subject to'	9304:-1:u'/Talk'	9464:1:u'\\n'
+"""
+    val d = n3daemon(doc)
       
     it("should process simple string queries to get rev_ids") {
       val q = ("q" -> "\"/Todo\"")~("fields" -> "rev_id")
@@ -131,6 +133,27 @@ class SearcherDaemonSpec extends FunSpec {
       assert(JString("18201") == (json \ "hits")(0)(0))
     }
 
+    val d_s1 = daemon(new SimpleNGramAnalyzer(1), doc)
+    val d_h1 = daemon(new HashedNGramAnalyzer(1, 2, 9876), doc)
+
+    def phraseQueries(name:String, address: InetSocketAddress) {
+      it(f"should process non-phrase queries (${name})") {
+        val q = ("q" -> "subject cover") ~ ("fields" -> "rev_id")
+        val json = retrieve(address, q)
+        assert(JInt(2) == json \ "hits_all")
+      }
+
+      it(f"should process phrase queries (${name})") {
+        val q = ("q" -> "\"subject cover\"") ~ ("fields" -> "rev_id")
+        val json = retrieve(address, q)
+        assert(JInt(1) == json \ "hits_all")
+        assert(JString("233192") == (json \ "hits")(0)(0))
+      }
+    }
+
+    phraseQueries("simple ngram", d_s1.address)
+    phraseQueries("hashed ngram", d_h1.address)
+
   }
 
   describe("SearcherDaemon with three small documents") {
@@ -144,8 +167,8 @@ class SearcherDaemonSpec extends FunSpec {
       val q = ("q" -> "rev_id:12345 OR page_id:12") ~ ("fields", "rev_id")
       val json = retrieve(d.address, q)
       assert(JInt(2) == (json \ "hits_all"))
-      assert(Set(JString("12345"), JString("18201")) ==
-        Set((json \ "hits")(0)(0), (json \ "hits")(1)(0)))
+      assert(Set("12345", "18201") ==
+        (json \ "hits" \\ classOf[JString]).toSet)
     }
 
     it("should process '?' queries") {
@@ -153,6 +176,48 @@ class SearcherDaemonSpec extends FunSpec {
       val json = retrieve(d.address, q)
       assert(JInt(1) == json \ "hits_all")
       assert(JString("233192") == (json \ "hits")(0)(0))
+    }
+
+  }
+
+  describe("SearcherDaemon with another three small documents") {
+
+    val d = n1daemon("""233192	10	0	'Accessiblecomputing'	980043141	u'*'	False	99	u'RoseParks'	0:1:u'This subject covers\\n\\n* AssistiveTechnology\\n\\n* AccessibleSoftware\\n\\n* AccessibleWeb\\n\\n* LegalIssuesInAccessibleComputing\\n\\n'
+18201	12	0	'Anarchism'	1014649222	u'Automated conversion'	True	None	u'Conversion script'	9230:1:u'[[talk:Anarchism|'	9252:1:u']]'	9260:1:u'[[Anarchism'	9276:1:u'|/Todo]]'	9292:1:u'talk:'	9304:-1:u'/Talk'	9464:1:u'\\n'
+18210	12	0	'Anarchism'	1014749333	u'Automated conversion'	True	None	u'Conversion script'	9230:1:u'[[talk:Anarchism|'	9252:1:u']]'	9260:1:u'[[Anarchism'	9276:1:u'|/Todo]]'	9292:1:u'talk:'	9304:-1:u'/Talk'	9464:1:u'\\n'
+""")
+
+    it("should produce collapsed hits by month") {
+      val q = ("q" -> "\"/Todo\"") ~ ("fields" -> "rev_id") ~ ("collapse_hits" -> "month")
+      val json = retrieve(d.address, q)
+      
+      assert(JInt(2) == json \ "hits_all")
+      assert(JString("2002-02") == (json \ "hits")(0)(0))
+      assert(List("18201", "18210") == (json \ "hits")(0)(1) \\ classOf[JString])
+    }
+
+    it("should produce collapsed hits by day") {
+      val q = ("q" -> "\"/Todo\"") ~ ("fields" -> "rev_id") ~ ("collapse_hits" -> "day")
+      val json = retrieve(d.address, q)
+      
+      assert(JInt(2) == json \ "hits_all")
+      assert(JString("2002-02-25") == (json \ "hits")(0)(0))
+      assert(JString("2002-02-26") == (json \ "hits")(1)(0))
+      assert(JString("18201") == (json \ "hits")(0)(1)(0)(0))
+      assert(JString("18210") == (json \ "hits")(1)(1)(0)(0))
+      assert(List("18201", "18210") ==
+        (for ( JArray(List(JString(_), y)) <- json; JString(z) <- y ) yield z))
+    }
+
+    it("should produce collapsed hit counts by day") {
+      val q = ("q" -> "\"/Todo\"") ~ ("collapse_hits" -> "day")
+      val json = retrieve(d.address, q)
+      
+      assert(JInt(2) == json \ "hits_all")
+      assert(JString("2002-02-25") == (json \ "hits")(0)(0))
+      assert(JString("2002-02-26") == (json \ "hits")(1)(0))
+      assert(JInt(1) == (json \ "hits")(0)(1))
+      assert(JInt(1) == (json \ "hits")(1)(1))
     }
 
   }
